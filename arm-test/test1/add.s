eae854cef863f2e9ca09f250537992eee6ba9c38
Pavel Zaichenkov <zaichenkov@gmail.com>
2011-09-22 09:13:38 +0100

    Test files are updated. Log is added.

--- arm-test/test1/add.s
+++ arm-test/test1/add.s
@@ -1,8 +1,8 @@
 
 start:
-@  mov   r0, #5	      @ Load register r0 with the value 5
-@  mov   r1, #4	      @ Load register r1 with the value 4
-@  add   r2, r1, r0    @ Add r0 and r1 and store in r2
+  mov   r0, #5	      @ Load register r0 with the value 5
+  mov   r1, #4	      @ Load register r1 with the value 4
+  add   r2, r1, r0    @ Add r0 and r1 and store in r2
   mrs	r3, cpsr      @ Move cpsr value to general register r3
 
 stop:
